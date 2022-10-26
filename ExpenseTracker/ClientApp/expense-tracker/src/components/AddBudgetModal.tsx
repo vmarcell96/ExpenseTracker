df95feef37e4ca26364735e7b922cd075e21ed49
vmarcell96 <vajdam96@gmail.com>
2022-10-26 16:03:25 +0200

    Created modal component for adding budgets

---
+++ ExpenseTracker/ClientApp/expense-tracker/src/components/AddBudgetModal.tsx
@@ -0,0 +1,58 @@
+import { Button, Form, Modal } from "react-bootstrap"
+import { useRef } from "react"
+import {useBudgets} from '../contexts/BudgetContext'
+
+
+type AddBudgetModalProps = {
+    show: boolean,
+    handleClose: () => void,
+}
+
+export default function AddBudgetModal({ show, handleClose }: AddBudgetModalProps) {
+
+    const nameRef = useRef<HTMLInputElement>(null);
+    const maxRef = useRef<HTMLInputElement>(null);
+    const { addBudget } = useBudgets();
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        if (maxRef.current && nameRef.current) {
+            addBudget({
+                name: nameRef.current.value,
+                max: parseFloat(maxRef.current.value)
+        })
+        }
+        handleClose();
+    }
+
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Form onSubmit={handleSubmit}>
+
+                <Modal.Header closeButton>
+                    <Modal.Title>New Budget</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group className="mb-3" controlId="name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control ref={nameRef} type="text" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="max">
+                        <Form.Label>Maximum Spending</Form.Label>
+                        <Form.Control 
+                            ref={maxRef} 
+                            type="number" 
+                            required 
+                            min={0} 
+                            step={1} />
+                    </Form.Group>
+                    <div className="d-flex justify-content-end">
+                        <Button variant="primary" type="submit">
+                            Add
+                        </Button>
+                    </div>
+                </Modal.Body>
+            </Form>
+        </Modal>
+    )
+}
