589d90347ca92a809a5ecf46ba1cfdb299f6a7e8
vmarcell96 <vajdam96@gmail.com>
2022-10-26 16:02:52 +0200

    created BudgetCard component for showing Budgets

--- ExpenseTracker/ClientApp/expense-tracker/src/components/BudgetCard.tsx
+++ ExpenseTracker/ClientApp/expense-tracker/src/components/BudgetCard.tsx
@@ -2,35 +2,58 @@ import { Button, Card, ProgressBar, Stack } from "react-bootstrap";
 import { currencyFormatter } from "../utils"
 
 
-interface BudgetCardProps {
+type BudgetCardProps = {
     name: string;
     amount: number;
-    max: number;
+    max?: number;
+    gray: boolean;
+    onAddExpenseClick: () => void;
 }
 
-export default function BudgetCard({ name, amount, max }: BudgetCardProps) {
+export default function BudgetCard({ name, amount, max, gray, onAddExpenseClick }: BudgetCardProps) {
+    const classNames = [];
+    if (max) {
+        if (amount > max) {
+            classNames.push("bg-danger", "bg-opacity-10")
+        }
+    }
+    if (gray && classNames.length === 0) {
+        classNames.push("bg-light");
+    }
+
+
     return (
-        <Card>
+        <Card className={classNames.join(" ")}>
             <Card.Body>
                 <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
                     <div className="me-2">{name}</div>
                     <div className="d-flex align-items-baseline">
                         {currencyFormatter.format(amount)}
-                        <span className="text-muted fs-6 ms-1">
+                        {max && <span className="text-muted fs-6 ms-1">
                             / {currencyFormatter.format(max)}
-                        </span>
+                        </span>}
                     </div>
                 </Card.Title>
-                <ProgressBar 
-                className="rounded-pill" 
-                variant={getProgressBarVariant(amount, max)}
-                min={0}
-                max={max}
-                now={amount}
-                 />
+                {max && <ProgressBar
+                    className="rounded-pill"
+                    variant={getProgressBarVariant(amount, max)}
+                    min={0}
+                    max={max}
+                    now={amount}
+                />}
                 <Stack direction="horizontal" gap={2} className="mt-4">
-                    <Button>Add Expense</Button>
-                    <Button>View Expenses</Button>
+                    <Button
+                        variant="outline-primary"
+                        className="ms-auto"
+                        onClick={onAddExpenseClick}
+                    >
+                        Add Expense
+                    </Button>
+                    <Button
+                        variant="outline-secondary"
+                    >
+                        View Expenses
+                    </Button>
                 </Stack>
             </Card.Body>
         </Card>
@@ -42,5 +65,4 @@ function getProgressBarVariant(amount: number, max: number): string {
     if (ratio < .5) return "primary";
     if (ratio < .75) return "warning";
     return "danger";
-
 }
