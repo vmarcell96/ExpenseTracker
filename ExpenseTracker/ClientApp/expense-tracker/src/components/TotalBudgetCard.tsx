d7f7e81136286b1a17e23793a6c2c6edbc435492
vmarcell96 <vajdam96@gmail.com>
2022-10-26 18:33:38 +0200

    TotalBudgetCard components buttons are hidden

--- ExpenseTracker/ClientApp/expense-tracker/src/components/TotalBudgetCard.tsx
+++ ExpenseTracker/ClientApp/expense-tracker/src/components/TotalBudgetCard.tsx
@@ -1,17 +1,13 @@
 import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "../contexts/BudgetContext";
 import BudgetCard from "./BudgetCard";
 
-type UncategorizedBudgetCardProps = {
-    onAddExpenseClick: () => void;
-}
-
-export default function TotalBudgetCard({onAddExpenseClick}: UncategorizedBudgetCardProps) {
+export default function TotalBudgetCard() {
 
     const { expenses, budgets } = useBudgets();
     const amount = expenses.reduce((partsum, exp) => partsum + exp.amount, 0)
     const max = budgets.reduce((partsum, budget) => partsum + budget.max, 0)
     if (max === 0) return null;
     return (
-        <BudgetCard amount={amount} max={max} name="Total" gray={true} onAddExpenseClick={onAddExpenseClick}/>
+        <BudgetCard amount={amount} max={max} name="Total" gray={true} hideButtons={true}/>
     )
 }
